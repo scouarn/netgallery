1c1fee8bf506dbc79f04074290ca69b2319a3cc1
scouarn <scouarn.contact@gmail.com>
2022-01-11 21:39:16 +0100

    template

---
+++ pkg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [ -n "$1" ]; then
+	VERSION="$1"
+
+else
+	# https://stackoverflow.com/questions/55730793/how-can-i-increment-a-number-at-the-end-of-a-string-in-bash
+	[[ $(cat version) =~ ([0-9]+)\.([0-9]+) ]] || { echo 'invalid input'; exit; }
+
+    VERSION="${BASH_REMATCH[1]}.$(( ${BASH_REMATCH[2]} + 1 ))"
+fi
+
+echo "New version is $VERSION" &&
+echo $VERSION > version &&
+
+zip -r "release/v$VERSION.zip" ./*
